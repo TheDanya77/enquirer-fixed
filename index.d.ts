44b06e7f88226024d760bcf1adcf0d49af8204b5
TheDanya77 <nesterenkodanya2@gmail.com>
2025-11-15 15:58:48 +0200

    Type fixed again

--- index.d.ts
+++ index.d.ts
@@ -38,13 +38,13 @@ interface BasePromptOptions {
 }
 
 export interface Choice {
-  name: string
-  message?: string
-  value?: unknown
-  hint?: string
-  role?: string
-  enabled?: boolean
-  disabled?: boolean | string
+    name: string
+    message?: string
+    value?: unknown
+    hint?: string
+    role?: string
+    enabled?: boolean
+    disabled?: boolean | string
 }
 
 export type ArrayTypes = 'autocomplete'
@@ -57,59 +57,59 @@ export type ArrayTypes = 'autocomplete'
     | 'scale'
 
 export interface SelectPromptOptions extends BasePromptOptions {
-  type: ArrayTypes
-  choices: (string | Choice)[]
-  maxChoices?: number
-  multiple?: boolean
-  initial?: number
-  delay?: number
-  separator?: boolean
-  sort?: boolean
-  linebreak?: boolean
-  edgeLength?: number
-  align?: 'left' | 'right'
-  scroll?: boolean,
-  pointer?: string
+    type: ArrayTypes
+    choices: (string | Choice)[]
+    maxChoices?: number
+    multiple?: boolean
+    initial?: number
+    delay?: number
+    separator?: boolean
+    sort?: boolean
+    linebreak?: boolean
+    edgeLength?: number
+    align?: 'left' | 'right'
+    scroll?: boolean,
+    pointer?: string
 }
 
 interface BooleanPromptOptions extends BasePromptOptions {
-  type: 'confirm'
-  initial?: boolean
+    type: 'confirm'
+    initial?: boolean
 }
 
 export type StringTypes = 'input' | 'invisible' | 'list' | 'password' | 'text'
 
 export interface StringPromptOptions extends BasePromptOptions {
-  type: StringTypes
-  initial?: string
-  multiline?: boolean
+    type: StringTypes
+    initial?: string
+    multiline?: boolean
 }
 
 export type NumberTypes = "numeral"
 
 export interface NumberPromptOptions extends BasePromptOptions {
-  type: NumberTypes
-  min?: number
-  max?: number
-  delay?: number
-  float?: boolean
-  round?: boolean
-  major?: number
-  minor?: number
-  initial?: number
+    type: NumberTypes
+    min?: number
+    max?: number
+    delay?: number
+    float?: boolean
+    round?: boolean
+    major?: number
+    minor?: number
+    initial?: number
 }
 
 interface SnippetPromptOptions extends BasePromptOptions {
-  type: 'snippet'
-  newline?: string
-  template?: string
+    type: 'snippet'
+    newline?: string
+    template?: string
 }
 
 interface SortPromptOptions extends BasePromptOptions {
-  type: 'sort'
-  hint?: string
-  drag?: boolean
-  numbered?: boolean
+    type: 'sort'
+    hint?: string
+    drag?: boolean
+    numbered?: boolean
 }
 
 interface ValuePrompt<T = any> extends Prompt {
@@ -121,13 +121,13 @@ interface ValuePrompt<T = any> extends Prompt {
 }
 
 export type PromptOptions =
-  | BasePromptOptions
-  | SelectPromptOptions
-  | BooleanPromptOptions
-  | StringPromptOptions
-  | NumberPromptOptions
-  | SnippetPromptOptions
-  | SortPromptOptions
+    | BasePromptOptions
+    | SelectPromptOptions
+    | BooleanPromptOptions
+    | StringPromptOptions
+    | NumberPromptOptions
+    | SnippetPromptOptions
+    | SortPromptOptions
 
 declare class BasePrompt extends EventEmitter {
     state: Record<string, any>;
@@ -143,43 +143,43 @@ declare class BasePrompt extends EventEmitter {
     clear(): void;
     cursorHide(): void;
     cursorShow(): void;
-  }
+}
 
 declare class Enquirer<T = object> extends EventEmitter {
-  constructor(options?: object, answers?: T);
+    constructor(options?: object, answers?: T);
 
-  /**
-   * Register a custom prompt type.
-   *
-   * @param type
-   * @param fn `Prompt` class, or a function that returns a `Prompt` class.
-   */
-  register(type: string, fn: typeof BasePrompt | (() => typeof BasePrompt)): this;
+    /**
+     * Register a custom prompt type.
+     *
+     * @param type
+     * @param fn `Prompt` class, or a function that returns a `Prompt` class.
+     */
+    register(type: string, fn: typeof BasePrompt | (() => typeof BasePrompt)): this;
 
-  /**
-   * Register a custom prompt type.
-   */
-  register(type: { [key: string]: typeof BasePrompt | (() => typeof BasePrompt) }): this;
+    /**
+     * Register a custom prompt type.
+     */
+    register(type: { [key: string]: typeof BasePrompt | (() => typeof BasePrompt) }): this;
 
-  /**
-   * Prompt function that takes a "question" object or array of question objects,
-   * and returns an object with responses from the user.
-   *
-   * @param questions Options objects for one or more prompts to run.
-   */
-  prompt(
-    questions:
-      | PromptOptions
-      | ((this: Enquirer) => PromptOptions)
-      | (PromptOptions | ((this: Enquirer) => PromptOptions))[]
-  ): Promise<T>;
+    /**
+     * Prompt function that takes a "question" object or array of question objects,
+     * and returns an object with responses from the user.
+     *
+     * @param questions Options objects for one or more prompts to run.
+     */
+    prompt(
+        questions:
+            | PromptOptions
+            | ((this: Enquirer) => PromptOptions)
+            | (PromptOptions | ((this: Enquirer) => PromptOptions))[]
+    ): Promise<T>;
 
-  /**
-   * Use an enquirer plugin.
-   *
-   * @param plugin Plugin function that takes an instance of Enquirer.
-   */
-  use(plugin: (this: this, enquirer: this) => void): this;
+    /**
+     * Use an enquirer plugin.
+     *
+     * @param plugin Plugin function that takes an instance of Enquirer.
+     */
+    use(plugin: (this: this, enquirer: this) => void): this;
 }
 
 export interface KeyPressed {
@@ -193,49 +193,53 @@ export interface KeyPressed {
 }
 
 declare namespace Enquirer {
-  function prompt<T = object>(questions: PromptOptions): Promise<T>;
+    function prompt<T = object>(questions: PromptOptions): Promise<T>;
 
-  class Prompt<T = any> extends BasePrompt {
-      value?: T;
-      v?: T;
-      prefix?: string;
-      suffix?: string;
-      highlight?: (choice: string) => string;
-      pointer?: string;
-      symbols?: Record<string, string>;
+    class Prompt<T = string> extends BasePrompt {
+        clientValue!: T;
+        value!: T;
+        prefix?: string;
+        suffix?: string;
+        highlight?: (choice: string) => string;
+        pointer?: string;
+        symbols?: Record<string, string>;
 
-      constructor(options: PromptOptions) {
+        constructor(options: PromptOptions) {
 
-          this.on("keypress", (ch: string, key: KeyPressed) => this.onKeypress(ch, key));
-      };
+            super(options);
+            this.clientValue = this.options.initial || ""
+            this.on("keypress", (ch: string, key: KeyPressed) => this.onKeypress(ch, key));
+        };
 
-      up(): void {}
-      down(): void {}
-      left(): void {}
-      right(): void {}
+        up(): void {}
+        down(): void {}
+        left(): void {}
+        right(): void {}
 
-      render(): void {}
-      run(): Promise<T> {
-          return Promise.resolve(this.value);
-      }
+        render(): void {}
+        run(): Promise<T> {
+            return Promise.resolve(this.value);
+        }
 
-      submit(): void {}
-      cancel(): void {}
-      clear(): void {}
-      write(str: string): void {}
-      cursorHide(): void {}
-      cursorShow(): void {}
+        submit(): void {}
+        cancel(): void {}
+        clear(): void {}
+        write(str: string): void {}
+        cursorHide(): void {}
+        cursorShow(): void {}
 
-      onKeypress(ch: string, key: KeyPressed): void {
-          if (key.name === "return") this.submit();
-      }
-      onSubmit(name: string, value: any): boolean | Promise<boolean> {
-          return true;
-      }
-      onCancel(name: string, value: any): boolean | Promise<boolean> {
-          return false;
-      }
-  }
+        onKeypress(ch: string, key: KeyPressed): void {
+            if (key.name === "return") this.submit();
+        }
+        onSubmit(name: string, value: any): boolean | Promise<boolean> {
+            return true;
+        }
+        onCancel(name: string, value: any): boolean | Promise<boolean> {
+            return false;
+        }
+    }
+
+
 }
 
 export = Enquirer;
